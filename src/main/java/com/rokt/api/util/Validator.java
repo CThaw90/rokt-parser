e527994ea016169f185aa310e514057a43565165
CThaw90 <christhaw90@gmail.com>
2022-04-04 15:02:40 -0400

    Updated regex validation for ISO format to enforce four digits for year

--- src/main/java/com/rokt/api/util/Validator.java
+++ src/main/java/com/rokt/api/util/Validator.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class Validator {
 
-    private static final String ISO_REGEX_STRING = "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$";
+    private static final String ISO_REGEX_STRING = "^[0-9]{4}-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$";
 
     public static Boolean isISOFormat(String value) {
         return value != null && value.matches(ISO_REGEX_STRING);
